334dd3947b3f8df1c29f0f63f732572748d37512
eetann <hideharu092@gmail.com>
2023-03-07 21:45:04 +0900

    feat: update

--- validate.ts
+++ validate.ts
@@ -11,14 +11,20 @@ import path from "path";
 import { readdirSync, readFileSync } from "fs";
 import { customLinkFetchJsonSchema } from "./choomame/src/features/customLink/customLinkSchema.js";
 
+let hasError = false;
+
 readdirSync("./src").forEach((file) => {
   if (file.endsWith(".json5")) {
     const filepath = path.join("./src", file);
     const data = JSON5.parse(readFileSync(filepath, "utf8"));
     const parseResult = customLinkFetchJsonSchema.safeParse(data);
     if (parseResult.success === false) {
-      console.error(parseResult.error.issues);
-      throw new Error("Parsing failed. Please check the schema.");
+      hasError = true;
+      console.log(`Error: ${filepath}`);
+      console.log(parseResult.error.issues);
     }
+  }
+  if (hasError) {
+    throw new Error("Parsing failed. Please check the schema.");
   }
 });
